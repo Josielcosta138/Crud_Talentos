7e57106eb2367d8f161c728b2c39aa84efb07df3
ramonlindao <ramon_joaquim1@hotmail.com>
2023-07-02 19:10:47 -0300

    ajustado o erro no cancelar e botao do x na tela de login e da tela de senha

--- src/src/model/Login.java
+++ src/src/model/Login.java
@@ -9,21 +9,9 @@ import static modelController.ProcessosGerais.chamaMenuPrincipal;
 
 public class Login {
 
- public static void exibirTelaLogin() throws SQLException, ClassNotFoundException {
-     Object usuarioLogado = chamaSelecaoUsuario();
-     checaSenhaUsuario(usuarioLogado);
-    }
-    private static void checaSenhaUsuario(Object usuarioLogado) throws SQLException, ClassNotFoundException {
-        String senhaDigitada = JOptionPane.showInputDialog(null,
-                "Informe a senha do usuário: ");
-        Usuario usuarioByLogin = UsuarioDao.findUsuarioByLogin((String) usuarioLogado);
-
-        if (usuarioByLogin.getSenha().equals(senhaDigitada)) {
-            chamaMenuPrincipal();
-        } else {
-            JOptionPane.showMessageDialog(null, "Senha incorreta! Digite novamente: ");
-            checaSenhaUsuario(usuarioLogado);
-        }
+    public static void exibirTelaLogin() throws SQLException, ClassNotFoundException {
+        Object usuarioLogado = chamaSelecaoUsuario();
+        checaSenhaUsuario(usuarioLogado);
     }
 
     private static Object chamaSelecaoUsuario() {
@@ -31,8 +19,25 @@ public class Login {
         String initialSelection = (String) selectionValues[0];
         Object selection = JOptionPane.showInputDialog(null, "Selecione o usuário: ",
                 "Tela de Login", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
-        return selection;
-    }
+        if (selection == null) {
+            int opcaoSair = JOptionPane.showOptionDialog(null, "Você está cancelando o login. Deseja realmente sair ?",
+                    "Confirmação", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+            if(opcaoSair == JOptionPane.YES_NO_OPTION)
+            System.exit(0);
+        chamaSelecaoUsuario();}
+            return selection;
+        }
 
-
+        private static void checaSenhaUsuario (Object usuarioLogado) throws SQLException, ClassNotFoundException {
+            String senhaDigitada = JOptionPane.showInputDialog(null,
+                    "Informe a senha do usuário: ");
+            Usuario usuarioByLogin = UsuarioDao.findUsuarioByLogin((String) usuarioLogado);
+if (senhaDigitada == null){
+    chamaSelecaoUsuario();
 }
+            if (usuarioByLogin.getSenha().equals(senhaDigitada)) {
+                chamaMenuPrincipal();
+            } else {
+                JOptionPane.showMessageDialog(null, "Senha incorreta! Digite novamente: ");
+                checaSenhaUsuario(usuarioLogado);
+            }}}
